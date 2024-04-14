6d1414edeee9cafab131164b9b904b3f81c690fa
Григорий Кривобоков <danilazaiac@gmail.com>
2024-04-14 21:35:19 +0300

    Version 1.0

---
+++ ControlsWorker/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlsWorker
+{
+    public static class ControlsApplication
+    {
+
+        public static T CloneElement<T>(T prototype) where T : Control
+        {
+            T instance = Activator.CreateInstance<T>();
+            PropertyInfo[] info = typeof(T).GetProperties();
+
+            foreach(var i in info)
+            {
+                if(i.CanWrite && i.Name != "WindowTarget")
+                {
+                    i.SetValue(instance, i.GetValue(prototype, null), null);
+                }
+            }
+
+            return instance;
+        }
+
+        public static T CloneChild<T>(T prototype) where T : Control
+        {
+            T instance = CloneElement(prototype);
+
+            PropertyInfo[] propertyInfos = typeof(T).GetProperties();
+            bool continer = false;
+
+            foreach(var i in propertyInfos)
+            {
+                continer = i.Name == "Controls";
+                if (continer) break;
+            }
+
+            if(!continer) return instance;
+
+            foreach(Control i in prototype.Controls)
+            {
+                
+
+            }
+
+            return null;
+        }
+    }
+}
--- DataBaseWorker/Class1.cs
+++ DataBaseWorker/Class1.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.OleDb;
 using System.Data;
-using System.Windows.Forms;
+using System.Data.OleDb;
 using System.Drawing;
-using System.IO;
-using System.ComponentModel;
+using System.Windows.Forms;
 
 namespace DataBaseWorker
 {
